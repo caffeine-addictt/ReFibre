b07c5dd3a13f958ae109eeb7bcf082e93f1f5fe8
KenNoYu <tanjy0206@gmail.com>
2024-01-15 14:16:07 +0800

    feat: Sign in function

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
-from forms import SignUpForm
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
+from forms import SignUpForm, SignInForm
 import shelve, customer
+from werkzeug.security import generate_password_hash
 
-
+# Configure app
 app = Flask(__name__)
+
+# Configure session
+app.secret_key = 'hello'
+app.config["SESSION_PERMANENT"] = False
+
 
 # route for root path
 @app.route('/')
@@ -17,20 +24,62 @@ def signup():
     if request.method == 'POST' and sign_up.validate():
         customers_dict = {}
         db = shelve.open('customer_db')
+        email = request.form['email']
 
         try:
             customers_dict = db['Customers']
         except:
-            print("Error in retrieving Customers from customer.db.")
+            print("Error in retrieving Customers from customer_db.")
 
-        user = customer.Customer(sign_up.first_name.data, sign_up.last_name.data, sign_up.email.data, sign_up.password.data)
+        # validate if user account already exits
+        for users in customers_dict:
+            user = SignUpForm.account_validate(email)
+            # if email address is already in use
+            if user:
+                db.close()
+                return render_template('signUp_fail.html', form=sign_up)
+            # if email address not in use
+            if not user:
+                break
+
+        hashed_password = generate_password_hash(sign_up.password.data)
+        user = customer.Customer(sign_up.first_name.data, sign_up.last_name.data, sign_up.email.data, hashed_password)
         customers_dict[user.get_customer_id()] = user
         db['Customers'] = customers_dict
 
         db.close()
 
-        return redirect(url_for('display_customers'))
+        return redirect(url_for('signin'))
     return render_template('signUp.html', form=sign_up)
+
+# route for customers to log in
+@app.route('/signin',  methods=['GET', 'POST'])
+def signin():
+    sign_in = SignInForm(request.form)
+    if request.method == 'POST' and sign_in.validate():
+        customers_dict = {}
+        db = shelve.open('customer_db')
+        email = sign_in.email.data
+        password = sign_in.password.data
+
+        try:
+            customers_dict = db['Customers']
+        except:
+            print("Error in retrieving Customers from customer_db.")
+
+        if sign_in.account_authenticate(email, password):
+            session["user"] = email
+            return render_template('home.html', form=sign_in)
+        else:
+            return render_template('signIn_fail.html', form=sign_in)
+        
+    return render_template('signIn.html', form=sign_in)
+
+# route for customer to sign out
+@app.route('/signout')
+def signout():
+    session["user"] = None
+    return redirect("/")
 
 # route to display customers
 @app.route('/displayCustomers')
@@ -75,6 +124,21 @@ def update_customer(id):
         update_user_form.password.data = user.get_password()
 
         return render_template('updateCustomer.html', form=update_user_form)
+    
+# Route to delete user
+@app.route('/deleteUser/<int:id>', methods=['POST'])
+def delete_user(id):
+    users_dict = {}
+    db = shelve.open('customer_db', 'w')
+    users_dict = db['Customers']
+
+    users_dict.pop(id)
+
+    db['Customers'] = users_dict
+    db.close()
+
+    return redirect(url_for('display_customers'))
+
 
 # remove CSRF protection for the time being
 app.config['WTF_CSRF_ENABLED'] = False
--- src/forms.py
+++ src/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, SubmitField
+from wtforms import StringField, PasswordField, EmailField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 import shelve
+from werkzeug.security import check_password_hash
 
 class SignUpForm(FlaskForm):
     first_name = StringField('First Name', [Length(min=1, max=150), DataRequired()])
@@ -14,4 +15,39 @@ class SignUpForm(FlaskForm):
     ])
     confirm_password = PasswordField('Confirm Password')
     button = SubmitField(label='Create Now')
-  
+
+    def account_validate(email):
+        db = shelve.open('customer_db', 'r')
+        existing_user = db['Customers']
+        existing_list = []
+        for key in existing_user:
+            user = existing_user.get(key)
+            existing_list.append(user)
+        for user in existing_list:
+            if email == user.get_email():
+                return True
+            else:
+                continue
+        return False
+            
+class SignInForm(FlaskForm):
+    email = EmailField('Email', [Email(), DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+    button = SubmitField(label="Log In")
+
+    def account_authenticate(self, email, password):
+        db = shelve.open('customer_db', 'r')
+        existing_user = db['Customers']
+        existing_list = []
+        for key in existing_user:
+            user = existing_user.get(key)
+            existing_list.append(user)
+        for user in existing_list:
+            if email == user.get_email():
+                user_pass = user.get_password()
+                return check_password_hash(user_pass, password)
+            elif email == "admin.refiber@gmail.com":
+                if password == "admin@password@1234":
+                    return True
+            else:
+                continue
