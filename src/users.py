a8308f7c7e1d1b999554bb9bb40bed28f881fa50
KenNuYu <tanjy0206@gmail.com>
2024-02-09 16:55:45 +0800

    feat: added more attributes to customers class

--- src/users.py
+++ src/users.py
@@ -3,17 +3,17 @@ import shelve
 # User class
 class User():
     def __init__(self, email, password):
-        self.__email = email
-        self.__password = password
+        self.email = email
+        self.password = password
     
     # Accessor method
     def get_email(self):
-        return self.__email
+        return self.email
     def get_password(self):
-        return self.__password
+        return self.password
     
     # Mutator methods
     def set_email(self, email):
-        self.__email = email
+        self.email = email
     def set_password(self, password):
-        self.__password = password
+        self.password = password
